770d05a62bb931d8df4a6ee5e662948a7b901318
Thapa <ujjwalthapa2307@gmail.com>
2020-04-21 18:36:12 +0900

    show a tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
