140c3c8d5bb6438b88b8da64de49215e63b3a236
Thapa <ujjwalthapa2307@gmail.com>
2020-04-23 17:01:56 +0900

    showing a destinations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag
+  get '/destinations/:id' => 'destinations#show', as: :destination
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
