c250be87763db06d08285fe99b934e88eeacdaf6
Thapa <ujjwalthapa2307@gmail.com>
2020-04-28 19:18:33 +0900

    Actor I but CSS is left to do

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get '/destinations/:id' => 'destinations#show', as: :destination
   get '/movies' => 'movies#index'
   get '/movies/:id' => 'movies#show', as: :movie
+  get '/actors' => 'actors#index'
+  get '/actors/:id' => 'actors#show', as: :actor
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
